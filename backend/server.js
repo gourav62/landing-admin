85366ba4f308bff756db55ce8bd8333cd3f2c37e
gourav62 <gouravgoswami7724@gmail.com>
2025-11-24 00:13:30 +0530

    Remove frontend serving from backend

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,11 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' })); // built-in body parser (supports large base64 images)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  // app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+  // });
 }
 
 
